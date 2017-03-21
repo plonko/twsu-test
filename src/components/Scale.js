aaf35a818888b37c97c44081169a71998aa13def
Seb <sebastianjthomas@gmail.com>
2017-03-21 11:59:18 +0000

    Tidied up Scale

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 
 function countInstances(words, thesauarus) {
-  const happyWordsCount = words.filter(function (e) {
-      return thesauarus.happy.indexOf(e.toLowerCase()) >= 0;
-  }).length;
+  // Turn object keys into array and return new array with count of words mapped to it
+  return Object.keys(thesauarus).map(key => words.filter(word => 
+    thesauarus[key].includes(word.toLowerCase())).length);
+}
 
-  const sadWordsCount = words.filter(function (e) {
-      return thesauarus.sad.indexOf(e.toLowerCase()) >= 0;
-  }).length;
-
-  return [happyWordsCount, sadWordsCount]
+function calcScale(arrayOfNums) {
+  const [happyCount, sadCount] = arrayOfNums;
+  const sum = happyCount + sadCount;
+  const scale = happyCount/sum || 0;
+  return scale;
 }
 
 const Scale = (props) => {
   const words = props.sentence.match(/\b(\w+)\b/g) || [];
   const thesauarus = props.thesauarus;
 
-  const [happyCount, sadCount] = countInstances(words, thesauarus);
-  const sum = happyCount + sadCount;
-  const scale = happyCount/sum || 0;
+  const count = countInstances(words, thesauarus);
+  const scale = calcScale(count);
 
   return <div>{scale}</div>
 }
